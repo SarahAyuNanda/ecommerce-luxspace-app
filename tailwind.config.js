8223a7cdf5b7ea1e0ab68a93d9c570eff9332331
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2023-03-08 11:36:40 +0700

    feat: add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss'), require('postcss'), require('autoprefixer')],
 }
