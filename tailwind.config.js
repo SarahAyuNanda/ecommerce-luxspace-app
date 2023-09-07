816661322f2f166e1a3e37d80b846bdb781110de
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2023-09-07 17:00:17 +0700

    chore: remove unused custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,6 @@ module.exports = {
         'orchid-pink': '#F6C7D6',
         'tuscany': '#BC93A4'
       },
-      boxShadow: {
-        'light': '-4px 9px 25px -6px rgba(0, 0, 0, 0.1)'
-      },
     },
   },
   plugins: [require('tailwindcss'), require('postcss'), require('autoprefixer')],
